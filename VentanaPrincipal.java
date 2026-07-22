5f6b923f388272c4ca255594287df8a9a1445025
Daniel Jimenez <0512025075@alumnos.unp.edu.pe>
2026-07-22 11:30:52 -0500

    Limpiar comentarios

--- VentanaPrincipal.java
+++ VentanaPrincipal.java
@@ -1,18 +1,7 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-
 package vista;
 
 import javax.swing.*;
 import java.awt.*;
-
-/**
- *
- * @author USUARIO
- */
-
 
 public class VentanaPrincipal extends JFrame {
 
@@ -22,13 +11,10 @@ public class VentanaPrincipal extends JFrame {
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setLocationRelativeTo(null);
 
-        // 1. Crear el lienzo
         PanelDiagrama panelDiagrama = new PanelDiagrama();
         
-        // 2. Crear la barra superior y conectarla al lienzo
         BarraHerramientas barra = new BarraHerramientas(panelDiagrama);
 
-        // 3. Agregar componentes a la ventana
         getContentPane().add(barra, BorderLayout.NORTH);
         getContentPane().add(panelDiagrama, BorderLayout.CENTER);
     }
