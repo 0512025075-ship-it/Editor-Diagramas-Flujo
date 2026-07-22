86c446f9e93b508205622c914ad37ed1c166d528
Daniel Jimenez <0512025075@alumnos.unp.edu.pe>
2026-07-22 10:29:42 -0500

    Limpiar comentarios

--- GestorFicheros.java
+++ GestorFicheros.java
@@ -1,7 +1,3 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package controlador;
 
 import modelo.Bloque;
@@ -9,14 +5,8 @@ import modelo.Conexion;
 import java.io.*;
 import java.util.List;
 
-/**
- *
- * @author USUARIO
- */
-
 public class GestorFicheros {
 
-    // Guarda el estado actual del diagrama (bloques y conexiones) en un archivo binario
     public static void guardar(File archivo, List<Bloque> bloques, List<Conexion> conexiones) throws IOException {
         try (ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream(archivo))) {
             oos.writeObject(bloques);
@@ -24,7 +14,6 @@ public class GestorFicheros {
         }
     }
 
-    // Lee el archivo y devuelve un objeto que contiene ambas listas
     @SuppressWarnings("unchecked")
     public static DatosDiagrama cargar(File archivo) throws IOException, ClassNotFoundException {
         try (ObjectInputStream ois = new ObjectInputStream(new FileInputStream(archivo))) {
@@ -34,7 +23,6 @@ public class GestorFicheros {
         }
     }
 
-    // Clase contenedora auxiliar para transportar ambos datos juntos
     public static class DatosDiagrama {
         public final List<Bloque> bloques;
         public final List<Conexion> conexiones;
