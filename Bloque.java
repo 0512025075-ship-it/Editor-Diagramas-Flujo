5b3c15ec84a21ae0e0ef4fbd5c4f01b39c302a4a
Daniel Jimenez <0512025075@alumnos.unp.edu.pe>
2026-07-24 12:18:02 -0500

    Implementar Hilos

--- Bloque.java
+++ Bloque.java
@@ -5,7 +5,7 @@ import java.io.Serializable;
 
 public class Bloque implements Serializable {
     private static final long serialVersionUID = 1L;
-  
+    
     public enum Tipo { 
         INICIO, PROCESO, DECISION, SALIDA, FIN 
     }
@@ -16,6 +16,7 @@ public class Bloque implements Serializable {
     private int x, y;
     private int ancho = 120;
     private int alto = 50;
+    private transient boolean ejecutando = false; //crea variable para uso en hilos
 
     public Bloque(String id, Tipo tipo, String texto, int x, int y) {
         this.id = id;
@@ -36,6 +37,14 @@ public class Bloque implements Serializable {
     public int getAncho() { return ancho; }
     public int getAlto() { return alto; }
 
+    //Métodos para los hilos
+    public boolean isEjecutando() { 
+        return ejecutando; 
+    }
+    public void setEjecutando(boolean ejecutando) { 
+        this.ejecutando = ejecutando; 
+    }
+    
     public boolean contienePunto(int px, int py) {
         return px >= x && px <= x + ancho && py >= y && py <= y + alto;
     }
