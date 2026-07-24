e7b05d911808749c4d903e2e38d03dee27bbfa55
Daniel Jimenez <0512025075@alumnos.unp.edu.pe>
2026-07-24 12:39:46 -0500

    Implementar Hilos

--- DibujadorBloques.java
+++ DibujadorBloques.java
@@ -9,7 +9,13 @@ public class DibujadorBloques {
     public static void dibujar(Graphics2D g2, Bloque b) {
         g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
 
-        g2.setColor(obtenerColor(b.getTipo()));
+        // 1. Pintar el fondo (Si está en ejecución por el hilo, se pinta de amarillo)
+        if (b.isEjecutando()) {
+            g2.setColor(Color.YELLOW); 
+        } else {
+            g2.setColor(obtenerColor(b.getTipo()));
+        }
+
         if (b.getTipo() == Bloque.Tipo.DECISION) {
             g2.fillPolygon(crearRombo(b));
         } else if (b.getTipo() == Bloque.Tipo.INICIO || b.getTipo() == Bloque.Tipo.FIN) {
@@ -18,8 +24,15 @@ public class DibujadorBloques {
             g2.fillRect(b.getX(), b.getY(), b.getAncho(), b.getAlto());
         }
 
-        g2.setColor(Color.BLACK);
-        g2.setStroke(new BasicStroke(2));
+        // 2. Dibujar el borde (Rojo si está ejecutando)
+        if (b.isEjecutando()) {
+            g2.setColor(Color.RED);
+            g2.setStroke(new BasicStroke(4));
+        } else {
+            g2.setColor(Color.BLACK);
+            g2.setStroke(new BasicStroke(2));
+        }
+
         if (b.getTipo() == Bloque.Tipo.DECISION) {
             g2.drawPolygon(crearRombo(b));
         } else if (b.getTipo() == Bloque.Tipo.INICIO || b.getTipo() == Bloque.Tipo.FIN) {
@@ -27,7 +40,8 @@ public class DibujadorBloques {
         } else {
             g2.drawRect(b.getX(), b.getY(), b.getAncho(), b.getAlto());
         }
-        
+
+        //Dibujar el texto
         g2.setColor(Color.BLACK);
         FontMetrics fm = g2.getFontMetrics();
         int textoX = b.getX() + (b.getAncho() - fm.stringWidth(b.getTexto())) / 2;
